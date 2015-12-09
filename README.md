bd93ff1545c4f2c125920b04c8824a06fbc37019
Avzgui <ri_antoine@yahoo.fr>
2015-12-09 14:58:14 +0100

    paste symfony test

---
+++ README.md
@@ -0,0 +1,4 @@
+Hibouc
+======
+
+A Symfony project created on December 8, 2015, 9:19 am.
---
+++ var/cache/dev/twig/c9/c901de58e1c54d4f4a8db0e5e9ff212d7dd5e3c0f7ed493c599819f2840c1e0c.php
@@ -0,0 +1,51 @@
+<?php
+
+/* LeHiboucBundle:Default:index.html.twig */
+class __TwigTemplate_ebaef120fd87833cd235acd1b5ab7bdfb6d2c14321ca5adb4fed84e374b18d9c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_fd802300ee476ed20541410eed3108a9c3e2a71070f3005d900a296544ae91c4 = $this->env->getExtension("native_profiler");
+        $__internal_fd802300ee476ed20541410eed3108a9c3e2a71070f3005d900a296544ae91c4->enter($__internal_fd802300ee476ed20541410eed3108a9c3e2a71070f3005d900a296544ae91c4_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "LeHiboucBundle:Default:index.html.twig"));
+
+        // line 2
+        echo "
+<html>
+  <body>
+    Hello World !
+  </body>
+</html>
+";
+        
+        $__internal_fd802300ee476ed20541410eed3108a9c3e2a71070f3005d900a296544ae91c4->leave($__internal_fd802300ee476ed20541410eed3108a9c3e2a71070f3005d900a296544ae91c4_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "LeHiboucBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,);
+    }
+}
+/* {# src/Avzgui/LeHiboucBundle/Resources/views/Default/index.html.twig #}*/
+/* */
+/* <html>*/
+/*   <body>*/
+/*     Hello World !*/
+/*   </body>*/
+/* </html>*/
+/* */
