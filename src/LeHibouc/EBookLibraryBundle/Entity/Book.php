d2fd08f7a99199b6390bc55e7873d1195f17f877
Avzgui <ri_antoine@yahoo.fr>
2015-12-22 17:17:04 +0100

    create Book entity

---
+++ src/LeHibouc/EBookLibraryBundle/Entity/Book.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace LeHibouc\EBookLibraryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity(repositoryClass="LeHibouc\EBookLibraryBundle\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descr", type="text")
+     */
+    private $descr;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="epub_file", type="string", length=255)
+     */
+    private $epubFile;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set descr
+     *
+     * @param string $descr
+     *
+     * @return Book
+     */
+    public function setDescr($descr)
+    {
+        $this->descr = $descr;
+
+        return $this;
+    }
+
+    /**
+     * Get descr
+     *
+     * @return string
+     */
+    public function getDescr()
+    {
+        return $this->descr;
+    }
+
+    /**
+     * Set epubFile
+     *
+     * @param string $epubFile
+     *
+     * @return Book
+     */
+    public function setEpubFile($epubFile)
+    {
+        $this->epubFile = $epubFile;
+
+        return $this;
+    }
+
+    /**
+     * Get epubFile
+     *
+     * @return string
+     */
+    public function getEpubFile()
+    {
+        return $this->epubFile;
+    }
+}
+
