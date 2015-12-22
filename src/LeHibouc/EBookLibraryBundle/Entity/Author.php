1ad2d14a6d4ccf036f5a08dd95b2899dfcd1a406
Avzgui <ri_antoine@yahoo.fr>
2015-12-22 17:37:14 +0100

    create Author entity

---
+++ src/LeHibouc/EBookLibraryBundle/Entity/Author.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace LeHibouc\EBookLibraryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Author
+ *
+ * @ORM\Table(name="author")
+ * @ORM\Entity(repositoryClass="LeHibouc\EBookLibraryBundle\Repository\AuthorRepository")
+ */
+class Author
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descr", type="text")
+     */
+    private $descr;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Author
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set descr
+     *
+     * @param string $descr
+     *
+     * @return Author
+     */
+    public function setDescr($descr)
+    {
+        $this->descr = $descr;
+
+        return $this;
+    }
+
+    /**
+     * Get descr
+     *
+     * @return string
+     */
+    public function getDescr()
+    {
+        return $this->descr;
+    }
+}
+
