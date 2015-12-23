29b2b3ba5ccc79ea7aa600b0460a7654ae863b74
Avzgui <ri_antoine@yahoo.fr>
2015-12-23 08:38:18 +0100

    rewrite entity book + library default controller

--- src/LeHibouc/EBookLibraryBundle/Controller/DefaultController.php
+++ src/LeHibouc/EBookLibraryBundle/Controller/DefaultController.php
@@ -3,11 +3,29 @@
 namespace LeHibouc\EBookLibraryBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('EBookLibraryBundle:Default:index.html.twig');
+    }
+
+
+    public function addBookAction()
+    {
+    	return new Response("Hello World ! (add)");
+    }
+    
+    public function viewBookAction($slug)
+    {
+    	return new Response("Hello World ! (book)");
+    }
+
+    public function viewBooksAction()
+    {
+    	return new Response("Hello World ! (books)");
     }
 }
