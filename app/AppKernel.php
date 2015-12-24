a841c1daf74f0ad3e7db1e07e65c6aeb7024dfe9
Avzgui <ri_antoine@yahoo.fr>
2015-12-24 11:53:11 +0100

    create User bundle and entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,9 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            //new FOS\UserBundle\FOSUserBundle(),
             new LeHibouc\AppBundle\AppBundle(),
             new LeHibouc\EBookLibraryBundle\EBookLibraryBundle(),
             new LeHibouc\KtbsBundle\KtbsBundle(),
+            new LeHibouc\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
